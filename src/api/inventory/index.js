8ad62dbea00a392fb1ae1a2b6a5f31badff1ee98
tipsyboy <gudah13@naver.com>
2025-11-20 17:14:28 +0900

    release

--- src/api/inventory/index.js
+++ src/api/inventory/index.js
@@ -4,7 +4,7 @@ const getProductInventoryList = async (page, size, request) => {
   const requestUrl = `/api/inventories/search?page=${page}&size=${size}`
   const requestUrlWithEs = `/api/inventories/search/es?page=${page}&size=${size}`
 
-  return await api.post(requestUrl, request)
+  return await api.post(requestUrlWithEs, request)
     .then((response) => {
       return response.data;
     })
